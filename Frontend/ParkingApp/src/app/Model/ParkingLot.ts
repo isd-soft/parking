f6abc956bd2c77617a37012b622a33c4a5c5369a
Agapii Mircea <mircea.agapii@gmail.com>
2020-01-07 13:04:08 +0200

    configure data.service for comunication with back endpoint

--- Frontend/ParkingApp/src/app/Model/ParkingLot.ts
+++ Frontend/ParkingApp/src/app/Model/ParkingLot.ts
@@ -3,4 +3,13 @@ export class ParkingLot {
   name: string;
   isFree: boolean;
   dateTime: Date;
+
+  static fromHttp(pl: ParkingLot): ParkingLot {
+    const newParkingLot = new ParkingLot();
+    newParkingLot.id = pl.id;
+    newParkingLot.name = pl.name;
+    newParkingLot.isFree = pl.isFree;
+    newParkingLot.dateTime = new Date(pl.dateTime);
+    return newParkingLot;
+  }
 }
--- Frontend/ParkingApp/src/app/data.service.ts
+++ Frontend/ParkingApp/src/app/data.service.ts
@@ -1,17 +1,25 @@
 import { Injectable } from '@angular/core';
 import { ParkingLot } from './Model/ParkingLot';
-import { Observable, of } from 'rxjs';
+import { Observable} from 'rxjs';
+import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  parkingLots: Array<ParkingLot>;
-
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
    getAllParkingLots(): Observable<Array<ParkingLot>> {
-     return of(null);
+     return this.http.get<Array<ParkingLot>>(environment.restUrl + '/parkingLots')
+     .pipe(
+       map(
+         data => data.map(
+           pl => ParkingLot.fromHttp(pl)
+         )
+       )
+     );
    }
 }
