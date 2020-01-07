ecadb8c5f97181cb7d18be007816e729aa9e97b1
aleks.g <al.guleac@gmail.com>
2020-01-07 15:27:39 +0200

    Added test H2 database

---
+++ Backend/src/test/java/com/isd/parking/ParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isd.parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
