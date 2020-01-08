eb9641011f082656efa0a8e5b60d91ce7f9d53f4
aleks.g <al.guleac@gmail.com>
2020-01-08 18:37:00 +0200

    added get and put methods
    @GetMapping("/parking/{id}") returns parking lot by id
    @PutMapping("/parking/{id}{status}") update time and status of parking lot by id

--- Backend/src/main/java/com/isd/parking/controller/ParkingLotController.java
+++ Backend/src/main/java/com/isd/parking/controller/ParkingLotController.java
@@ -1,12 +1,15 @@
 package com.isd.parking.controller;
 
+import com.isd.parking.exception.ResourceNotFoundException;
 import com.isd.parking.model.ParkingLot;
+import com.isd.parking.model.ParkingLotStatus;
 import com.isd.parking.service.ParkingLotService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -30,5 +33,31 @@ public class ParkingLotController {
     @GetMapping("/parking")
     public List<ParkingLot> getAllParkingLots() {
         return parkingLotService.listAll();
+    }
+
+
+    @GetMapping("/parking/{id}")
+    public ResponseEntity<ParkingLot> getEmployeeById(@PathVariable(value = "id") Long parkingLotId)
+            throws ResourceNotFoundException {
+        ParkingLot parkingLot = parkingLotService.findById(parkingLotId)
+                .orElseThrow(() -> new ResourceNotFoundException("Parking Lot not found for this id :: " + parkingLotId));
+
+        return ResponseEntity.ok().body(parkingLot);
+    }
+
+
+    @PutMapping("/parking/{id}{status}")
+    public ResponseEntity<ParkingLot> updateEmployee(@PathVariable(value = "id") Long parkingLotId,
+                                                     @PathVariable(value = "status") ParkingLotStatus status) throws ResourceNotFoundException {
+        ParkingLot parkingLot = parkingLotService.findById(parkingLotId)
+                .orElseThrow(() -> new ResourceNotFoundException("Parking Lot found for this id :: " + parkingLotId));
+
+        parkingLot.setStatus(status);
+
+        Date date = new Date(System.currentTimeMillis());
+        parkingLot.setUpdatedAt(date);
+
+        final ParkingLot updatedParkingLot = parkingLotService.save(parkingLot);
+        return ResponseEntity.ok(updatedParkingLot);
     }
 }
