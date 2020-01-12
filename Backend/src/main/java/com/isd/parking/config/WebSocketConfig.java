711ea80d10406c2b822215e569b091599f8f12e3
NoManEXE <weidmusic@gmail.com>
2020-01-13 00:11:21 +0200

    WebSocket now working with updated Backend

---
+++ Backend/src/main/java/com/isd/parking/config/WebSocketConfig.java
@@ -0,0 +1,16 @@
+package com.isd.parking.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new WebSocketHandler(), "/test");
+
+    }
+}
