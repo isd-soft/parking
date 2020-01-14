2b0bfc0e7a70bcc018897b02fbbf7200c839f716
Zhdanov Viktor <36110525+NoManEXE@users.noreply.github.com>
2020-01-14 23:39:33 +0200

    Update ParKing_laser_wifi.ino

--- Arduino_code/ParKing_wifi_alpha/ParKing_laser_wifi.ino
+++ Arduino_code/ParKing_wifi_alpha/ParKing_laser_wifi.ino
@@ -48,7 +48,7 @@ void setup() {
 
   Serial.println("Connected to Wifi, Connecting to server.");
   // try to connect to Websockets server
-  bool connected = client.connect(websockets_server_host, websockets_server_port, "/arduino/update");
+  bool connected = client.connect(websockets_server_host, websockets_server_port, "/arduino");
   if (connected) {
     Serial.println("Connected!");
 
