70f0f2c1f0f43edf959403e8f733a2e6f4721ecf
Zhdanov Viktor <36110525+NoManEXE@users.noreply.github.com>
2020-01-14 17:11:20 +0200

    new version of code, sending status only on state change

--- Arduino_code/ParKing_wifi_alpha/ParKing_laser_wifi.ino
+++ Arduino_code/ParKing_wifi_alpha/ParKing_laser_wifi.ino
@@ -1,9 +1,6 @@
 #include <ArduinoWebsockets.h>
+
 #include <WiFi.h>
-
-#define DETECT 2                // pin 2 for  sensor
-#define ACTION 8                // pin 8 for action to do something
-
 
 const char *ssid = "Inther";                            //Enter SSID
 const char *password = "inth3rmoldova";                 //Enter Password
@@ -12,11 +9,10 @@ const uint16_t websockets_server_port = 8080;           // Enter server port
 
 using namespace websockets;
 
+boolean isLotFree = false;                                              // boolean variable to define if the status of parking lot was changed or not
 WebsocketsClient client;
 
 int test_lot_number = 1;                                                //hardcoded test number
-
-boolean isLotFree = false;                                              // boolean variable to define if the status of parking lot was changed or not
 
 String status_free = "FREE";                                            //if string not compile use char *
 String status_occupied = "OCCUPIED";
@@ -24,11 +20,16 @@ String status_unknown = "UNKNOWN";
 
 char *security_token = "4a0a8679643673d083b23f52c21f27cac2b03fa2";      //some security token to verify connection ({SHA1}"arduino")
 
+
+const int pinLaser = 2; // output signal pin of laser module/laser pointer
+const int pinReceiver = 18; // input signal pin of receiver/detector (the used module does only return a digital state)
 void setup() {
-  Serial.begin(9600);
-  Serial.println("Laser Module Test");
-  pinMode(DETECT, INPUT);         //define detect input pin
-  pinMode(ACTION, OUTPUT);        //define ACTION output pin
+  pinMode(pinLaser, OUTPUT); // set the laser pin to output mode
+  pinMode(pinReceiver, INPUT); // set the laser pin to output mode
+  digitalWrite(pinLaser,  HIGH); // emit red laser
+  Serial.begin(9600); // Setup serial connection for print out to console
+  Serial.println("START");
+
 
   // Connect to wifi
   WiFi.begin(ssid, password);
@@ -47,7 +48,7 @@ void setup() {
 
   Serial.println("Connected to Wifi, Connecting to server.");
   // try to connect to Websockets server
-  bool connected = client.connect(websockets_server_host, websockets_server_port, "/arduino");
+  bool connected = client.connect(websockets_server_host, websockets_server_port, "/arduino/update");
   if (connected) {
     Serial.println("Connected!");
 
@@ -61,23 +62,21 @@ void setup() {
     Serial.println(message.data());
   });
 }
-
 void loop() {
+  int value = digitalRead(pinReceiver); // receiver/detector send either LOW or HIGH (no analog values!)
+  Serial.println(value); // send value to console
+  delay(100); // wait for 1000ms
 
-  int detected = digitalRead(DETECT);     // read Laser sensor
   char *msg = "{\"mBody\":\"Arduino data\", \"id\":\"";
 
-
-  if (detected == HIGH && isLotFree == false) {
-    digitalWrite(ACTION, HIGH);         // set the buzzer ON
-    Serial.println("Detected!");
-    isLotFree = true;
+  if (value == 0 && isLotFree == false) {
+    Serial.println("Lot is free!");
     client.send(msg + test_lot_number + String("\", \"status\":\"") + status_free + String("\", \"token\":\"") + security_token + String("\"}"));
+    isLotFree = true;
   }
 
-  if (detected == LOW && isLotFree == true) {
-    digitalWrite(ACTION, LOW);          // Set the buzzer OFF
-    Serial.println("No laser");
+  if (value == 1 && isLotFree == true) {
+    Serial.println("Lot is occupied!");
     client.send(msg + test_lot_number + String("\", \"status\":\"") + status_occupied + String("\", \"token\":\"") + security_token + String("\"}"));
     isLotFree = false;
   }
@@ -87,5 +86,5 @@ void loop() {
     client.poll();
   }
 
-  delay(200);
+  delay(500);
 }
