4e573e0c802a2e62fdc08d04df483ebf025fd064
Agapii Mircea <mircea.agapii@gmail.com>
2020-01-29 10:19:55 +0200

    arduino code for master-slave with libraries

---
+++ Arduino_code/ParKing_wifi_alpha/master-slave/Master/Master.ino
@@ -0,0 +1,212 @@
+/**
+* Master Arduino firmware
+*/
+
+
+#include <ArduinoWebsockets.h>
+#include <WiFi.h>
+
+#define ARRAY_SIZE(array) ((sizeof(array))/(sizeof(array[0])))  // used to calculate de length of an array
+
+#define MASTER_EN   5                                           // connected to RS485 Enable pin
+
+int slave_ids[] = {1, 2, 3};                                    // add new id when adding new slaves
+int sonar_ids[] = {1, 2, 3};                                    // add new id when will be added more sensors
+
+int active_sonars[9];                                           // active sensors with which will work master
+
+char slave_id;
+char sensor_id;
+String status_of_lot;
+String last_known_status[9];                                    // statuses of each sensor, index of the status must be the same as the id of sensor
+                                                                // change size when will be added more sensors
+
+//Wifi
+using namespace websockets;
+WebsocketsClient client;
+
+const char *ssid = "Inther";                                    // Enter SSID
+const char *password = "inth3rmoldova";                         // Enter Password
+const char *websockets_server_host = "104.248.250.89";          // Enter server address 
+const uint16_t websockets_server_port = 8080;                   // Enter server port
+
+//WebSocket
+//sample message for WebSocket
+char *msg = "{\"mBody\":\"Arduino data\", \"id\":\"";
+
+//security
+char *security_token = "4a0a8679643673d083b23f52c21f27cac2b03fa2"; //some security token to verify connection ({SHA1}"arduino")
+
+// timer to restart program every 60 min
+long timer;
+
+void(* resetFunc) (void) = 0;                                   // reseting the arduino board
+
+void setup() {
+  timer = millis();                                   
+
+  pinMode(MASTER_EN , OUTPUT);                                  // Declare Enable pin as output
+  Serial.begin(9600);                                           // set serial communication baudrate 
+  digitalWrite(MASTER_EN , LOW);                                // Make Enable pin low
+                                                                // Receiving mode ON 
+
+  // Connect to wifi
+    WiFi.begin(ssid, password);
+
+    // Wait some time to connect to wifi
+    for (int i = 0; i < 10 && WiFi.status() != WL_CONNECTED; i++)
+    {
+        Serial.print(".");
+        delay(1000);
+    }
+
+    // Check if connected to wifi
+    if (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.println("No Wifi!");
+        return;
+    }
+
+    Serial.println("Connected to Wifi, Connecting to server.");
+    // try to connect to WebSockets server
+    bool connected = client.connect(websockets_server_host, websockets_server_port, "/test");
+    if (connected)
+    {
+        Serial.println("Connected to WebSocket!");
+    }
+    else
+    {
+        Serial.println("Not Connected to WebSocket!");
+    }
+
+    // run callback when messages are received
+    client.onMessage([&](WebsocketsMessage message) {
+        Serial.print("Got Message: ");
+        Serial.println(message.data());
+    });
+
+    client.onEvent(onEventsCallback); 
+
+    // check which sensors are working
+    getActiveSonars();
+
+}
+
+void loop() {
+  String answer;
+  status_of_lot = ""; 
+    for (int i = 0; i < ARRAY_SIZE(active_sonars); i++) {
+      if (active_sonars[i]) {
+        digitalWrite(MASTER_EN , HIGH);                            // Make Enable pin high to send Data
+        delay(50);                                                 // required minimum delay of 5ms
+        Serial.println(active_sonars[i]);                          // Send slave and sensor id
+        Serial.flush();                                            // wait for transmission of data
+        digitalWrite(MASTER_EN , LOW);                             // Receiving mode ON
+      
+        delay(500);
+        while(Serial.available()) {
+          answer = Serial.readString();
+          answer.trim();
+        }
+      
+        slave_id = answer.charAt(0);
+        sensor_id = answer.charAt(1);
+        status_of_lot = answer.substring(2);
+        
+    
+        if (status_of_lot != "") {
+          if (status_of_lot != last_known_status[i]) {
+            client.send(msg +/* String(slave_id) + */String(sensor_id) + String("\", \"status\":\"") + status_of_lot + String("\", \"token\":\"") + security_token + String("\"}"));
+          }
+          last_known_status[i] = status_of_lot;
+        }
+      }
+    }
+  
+
+
+  if (client.available())
+    {
+        client.poll();
+        client.ping();
+
+    }
+
+   /// reset arduino after 1 hour    
+  if ((millis() - timer) > 1800000) {
+    resetFunc();
+  }
+}
+
+
+
+
+void onEventsCallback(WebsocketsEvent event, String data)
+{
+
+    if (event == WebsocketsEvent::ConnectionOpened)
+    {
+        Serial.println("WebSocket connection opened!");
+        client.send("test mess");
+    }
+    else if (event == WebsocketsEvent::ConnectionClosed)
+    {
+        Serial.println("WebSocket connection closed!");
+        Serial.println("Trying to reconnect to WebSocket ...");
+        delay(10000);
+        bool connected = client.connect(websockets_server_host, websockets_server_port, "/test");
+        resetFunc();
+
+        
+    }
+    else if (event == WebsocketsEvent::GotPing)
+    {
+        Serial.println("Got a Ping!");
+    }
+    else if (event == WebsocketsEvent::GotPong)
+    {
+//        Serial.println("Got a Pong!");
+    }
+}
+
+int getSize(int arr[]) {
+  int size = sizeof(arr)/sizeof(arr[0]);
+  return size;
+}
+
+void getActiveSonars() {
+  int index = 0;
+  int last_sonar;
+  String answer;
+  for (int j = 0; j < ARRAY_SIZE(slave_ids); j++) {
+    for (int i = 0; i < ARRAY_SIZE(sonar_ids); i++) {
+      if (sonar_ids[i]) {
+        digitalWrite(MASTER_EN , HIGH);                                // Make Enable pin high to send Data
+        delay(50);                                                     // required minimum delay of 5ms
+        Serial.print(slave_ids[j]); Serial.println(sonar_ids[i]);      // Send slave and sensor id
+        Serial.flush();                                                // wait for transmission of data
+        digitalWrite(MASTER_EN , LOW);                                 // Receiving mode ON
+      
+        delay(500);
+        while(Serial.available()) {
+          answer = Serial.readString();
+          answer.trim();
+        }
+      
+        slave_id = answer.charAt(0);
+        sensor_id = answer.charAt(1);
+        status_of_lot = answer.substring(2);
+        
+    
+        if (status_of_lot != "") {
+          int sonar = (slave_id - '0') * 10 + (sensor_id - '0');
+          if (sonar != last_sonar) {
+            active_sonars[index] = sonar;
+          }
+          index++;
+          last_sonar = sonar;
+        }
+      }
+    }
+  }
+}
---
+++ Arduino_code/ParKing_wifi_alpha/master-slave/libraries/ArduinoWebsockets/src/tiny_websockets/internals/ws_common.hpp
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <tiny_websockets/ws_config_defs.hpp>
+#include <string>
+#include <Arduino.h>
+
+namespace websockets {
+    typedef std::string WSString;
+    typedef String WSInterfaceString;
+
+    namespace internals {
+        WSString fromInterfaceString(const WSInterfaceString& str);
+        WSString fromInterfaceString(const WSInterfaceString&& str);
+        WSInterfaceString fromInternalString(const WSString& str);
+        WSInterfaceString fromInternalString(const WSString&& str);
+    }
+}
+
+#ifdef _WIN32
+    #include <tiny_websockets/network/windows/win_tcp_client.hpp>
+    #include <tiny_websockets/network/windows/win_tcp_server.hpp>
+    #define WSDefaultTcpClient websockets::network::WinTcpClient
+    #define WSDefaultTcpServer websockets::network::WinTcpServer
+
+    #ifndef _WS_CONFIG_NO_SSL
+        // OpenSSL Dependent
+        #include <tiny_websockets/network/openssl_secure_tcp_client.hpp>
+        #define WSDefaultSecuredTcpClient websockets::network::OpenSSLSecureTcpClient<WSDefaultTcpClient>
+    #endif //_WS_CONFIG_NO_SSL
+
+#elif defined(__linux__)
+    #include <tiny_websockets/network/linux/linux_tcp_client.hpp>
+    #include <tiny_websockets/network/linux/linux_tcp_server.hpp>
+    #define WSDefaultTcpClient websockets::network::LinuxTcpClient
+    #define WSDefaultTcpServer websockets::network::LinuxTcpServer
+
+    #ifndef _WS_CONFIG_NO_SSL
+        // OpenSSL Dependent
+        #include <tiny_websockets/network/openssl_secure_tcp_client.hpp>
+        #define WSDefaultSecuredTcpClient websockets::network::OpenSSLSecureTcpClient<WSDefaultTcpClient>
+    #endif //_WS_CONFIG_NO_SSL
+
+#elif defined(ESP8266)
+
+    #define PLATFORM_DOES_NOT_SUPPORT_BLOCKING_READ
+
+    #include <tiny_websockets/network/esp8266/esp8266_tcp.hpp>
+    #define WSDefaultTcpClient websockets::network::Esp8266TcpClient
+    #define WSDefaultTcpServer websockets::network::Esp8266TcpServer
+
+    #ifndef _WS_CONFIG_NO_SSL
+        // OpenSSL Dependent
+        #define WSDefaultSecuredTcpClient websockets::network::SecuredEsp8266TcpClient
+    #endif //_WS_CONFIG_NO_SSL
+
+#elif defined(ESP32)
+
+    #define PLATFORM_DOES_NOT_SUPPORT_BLOCKING_READ
+
+    #include <tiny_websockets/network/esp32/esp32_tcp.hpp>
+    #define WSDefaultTcpClient websockets::network::Esp32TcpClient
+    #define WSDefaultTcpServer websockets::network::Esp32TcpServer
+
+    #ifndef _WS_CONFIG_NO_SSL
+        // OpenSSL Dependent
+        #define WSDefaultSecuredTcpClient websockets::network::SecuredEsp32TcpClient
+    #endif //_WS_CONFIG_NO_SSL
+#endif
